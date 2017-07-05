d905a347db830ccbe0ce99644baa8021a92490cf
jorosoft <jorosoftbg@gmail.com>
2017-07-05 23:14:39 +0300

    add populate DB script for testing

---
+++ populate.js
@@ -0,0 +1,52 @@
+const constants = require('./common/constants');
+const repository = require('./data/repository')(constants);
+const data = require('./data')(repository);
+const models = require('./models')();
+
+let user = models.getUser('pesho', '1234', 'Петър', 'Стоянов');
+
+data.addUser(user)
+    .then((userId) => {
+        const doctor = models.getDoctor(userId, '12345', 'хирург');
+        data.addDoctor(doctor);
+    });
+
+user = models.getUser('stamat', '7777', 'Стамат', 'Киров');
+
+data.addUser(user)
+    .then((userId) => {
+        const doctor = models.getDoctor(userId, '56567', 'невролог');
+        data.addDoctor(doctor);
+    });
+
+user = models.getUser('pena', '0000', 'Пенка', 'Страхилова');
+
+data.addUser(user)
+    .then((userId) => {
+        const doctor = models.getDoctor(userId, '98765', 'дерматолог');
+        data.addDoctor(doctor);
+    });
+
+
+// /////////////////////////////////////////////////////////
+
+user = models.getUser('gosho', '1111', 'Георги', 'Димитров');
+
+data.addUser(user)
+    .then((userId) => {
+        const patient = models.getPatient(userId, '1212121212');
+        data.addPatient(patient);
+    });
+
+user = models.getUser('lalo', '1010', 'Лало', 'Костов');
+
+data.addUser(user)
+    .then((userId) => {
+        const patient = models.getPatient(userId, '9999999999');
+        data.addPatient(patient);
+    });
+
+
+// ////////////////////////////////////////////////
+
+console.log('DATA POPULATED!!!');
