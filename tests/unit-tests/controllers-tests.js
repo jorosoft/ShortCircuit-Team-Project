3a9e3437dc62561a12d85d1f7c8d54478163e062
jorosoft <jorosoftbg@gmail.com>
2017-07-28 11:41:23 +0300

    link targets in tests

--- tests/unit-tests/controllers-tests.js
+++ tests/unit-tests/controllers-tests.js
@@ -1,5 +1,11 @@
 const { expect } = require('chai');
 
+const authController = require('../../controllers/auth-controller');
+const homeController = require('../../controllers/home-controller');
+const patientController = require('../../controllers/patient-controller');
+const doctorController = require('../../controllers/doctor-controller');
+
+
 describe('Controllers Tests', () => {
     describe('Auth Controller Tests', () => {
 
--- tests/unit-tests/data-tests.js
+++ tests/unit-tests/data-tests.js
@@ -1,5 +1,12 @@
 const { expect } = require('chai');
 
+const authData = require('../../data/auth-data');
+const patientData = require('../../data/patient-data');
+const doctorData = require('../../data/doctor-data');
+const recipeData = require('../../data/recipe-data');
+const resultData = require('../../data/result-data');
+const reservationData = require('../../data/reservation-data');
+
 describe('Data Tests', () => {
     describe('Auth Data Tests', () => {
 
