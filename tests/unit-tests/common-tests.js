e7916b44068ad28517ea880f34066e9db81b99d5
jorosoft <jorosoftbg@gmail.com>
2017-07-29 16:53:14 +0300

    finish common tests

--- tests/unit-tests/common-tests.js
+++ tests/unit-tests/common-tests.js
@@ -23,14 +23,17 @@ describe('Common Tests', () => {
         it('expect validatePin() return false with invalid date', () => {
             const pinY = '0010126666';
             const pinM = '7788126666';
+            const pinMM = '7733126666';
             const pinD = '7710886666';
 
             const resultY = validator.validatePin(pinY);
             const resultM = validator.validatePin(pinM);
+            const resultMM = validator.validatePin(pinMM);
             const resultD = validator.validatePin(pinD);
 
             expect(resultY).to.be.false;
             expect(resultM).to.be.false;
+            expect(resultMM).to.be.false;
             expect(resultD).to.be.false;
         });
     });
