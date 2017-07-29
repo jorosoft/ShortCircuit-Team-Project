e6a4ac975d301fce5b109d1faa33091b5a0f3699
jorosoft <jorosoftbg@gmail.com>
2017-07-29 22:56:35 +0300

    another fix

--- routers/auth-router.js
+++ routers/auth-router.js
@@ -36,8 +36,7 @@ module.exports = function(app, data, models, constants) {
         .post('/register', controller.register)
         .get('/logout', controller.logout)
         .get('/profile', controller.getProfile)
-        .post('/profile', (req, res) => res.redirect('/profile-change'))
-        .get('/profile-change', controller.getChangeProfileForm)
+        .post('/profile', controller.getChangeProfileForm)
         .post('/profile-change', controller.changeProfileInfo)
         .get('/unauthorized', controller.unauthorized)
         .get('/user', passport
