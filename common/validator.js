dc69aabf4445948937e95277efcaec3b0271499b
jorosoft <jorosoftbg@gmail.com>
2017-07-29 16:46:41 +0300

    clear some unused code

--- common/validator.js
+++ common/validator.js
@@ -4,17 +4,6 @@ function isValidDate(y, m, d) {
 }
 
 module.exports = {
-    validateStringLength(value, minLength, maxLength) {
-        if (!value || typeof value !== 'string' ||
-            value.length < minLength || value.length > maxLength) {
-            throw new Error('Invalid input length!');
-        }
-    },
-    validatePasswordsMatch(pass, passConfirm) {
-        if (pass !== passConfirm) {
-            throw new Error('Password does not match password confirmation!');
-        }
-    },
     validatePin(pin) {
         if (pin.length !== 10) {
             return false;
