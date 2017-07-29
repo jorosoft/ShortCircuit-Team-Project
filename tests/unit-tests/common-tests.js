440f9a25d5d4f0f6baa57e0ce4ba0cfb10898b8c
jorosoft <jorosoftbg@gmail.com>
2017-07-29 10:47:29 +0300

    add validatePin tests

--- tests/unit-tests/common-tests.js
+++ tests/unit-tests/common-tests.js
@@ -1,5 +1,37 @@
 const { expect } = require('chai');
 
-describe('Common Tests', () => {
+const validator = require('../../common/validator');
 
+describe('Common Tests', () => {
+    describe('Validator Tests', () => {
+        it('expect validatePin() return false with invalid length pin', () => {
+            const pin = '123';
+
+            const result = validator.validatePin(pin);
+
+            expect(result).to.be.false;
+        });
+
+        it('expect validatePin() return false with invalid symbols pin', () => {
+            const pin = 'abc1234567';
+
+            const result = validator.validatePin(pin);
+
+            expect(result).to.be.false;
+        });
+
+        it('expect validatePin() return false with invalid date', () => {
+            const pinY = '0010126666';
+            const pinM = '7788126666';
+            const pinD = '7710886666';
+
+            const resultY = validator.validatePin(pinY);
+            const resultM = validator.validatePin(pinM);
+            const resultD = validator.validatePin(pinD);
+
+            expect(resultY).to.be.false;
+            expect(resultM).to.be.false;
+            expect(resultD).to.be.false;
+        });
+    });
 });
